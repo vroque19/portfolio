df33c0ed01414da38a7426c5a7a91f3b23678c49
Vanessa Roque <viroque19@gmail.com>
2025-01-01 03:51:31 +0000

    config files updated

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,36 @@
 <script>
+	import Nav from '$lib/components/Nav.svelte';
+	import { onMount } from 'svelte';
+	import Footer from '$lib/components/Footer.svelte';
+
 	import '../app.css';
 	let { children } = $props();
+	let isMobileMenuOpen = $state(false);
+	let isDarkMode = $state(false);
+
+	const checkDarkModeStorageAndPreference = () => {
+		// Check and apply the user's stored preference or the system preference on load
+		isDarkMode =
+			localStorage.theme === 'dark' ||
+			(!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches);
+	};
+
+	function toggleDarkMode() {
+		isDarkMode = !isDarkMode;
+		localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+	}
+
+	onMount(() => {
+		checkDarkModeStorageAndPreference();
+	});
 </script>
 
-{@render children()}
+<div class={isDarkMode ? 'dark' : 'light'}>
+	<main
+		class="min-h-screen w-full bg-neutral-one text-primary-three dark:bg-slate-800 dark:text-neutral-one"
+	>
+		<Nav bind:isMobileMenuOpen {toggleDarkMode} {isDarkMode} />
+		{@render children()}
+		<Footer />
+	</main>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,42 @@
 <script lang="ts">
-	import Nav from '$lib/components/Nav.svelte';
 	import Hero from '$lib/components/Hero.svelte';
 	import Projects from '$lib/components/Projects.svelte';
-	import Blog from '$lib/components/Blog.svelte';
 	import Contact from '$lib/components/Contact.svelte';
-	import Footer from '$lib/components/Footer.svelte';
-	import Carousel from '$lib/components/Carousel.svelte';
-
-	// State to control mobile menu
-	let isMobileMenuOpen: boolean = $state(false);
+	import Saos from 'saos';
 </script>
 
-<main class="min-h-screen w-full bg-neutral-one text-primary-three">
-	<Nav bind:isMobileMenuOpen />
-
-	<!-- Main content sections -->
-	<div class="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
-		<Hero />
-		<!-- <Carousel /> -->
+<!-- Main content sections -->
+<div class="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
+	<Hero />
+	<!-- <Carousel /> -->
+	<Saos
+		once={true}
+		animation={'slide-top 1.2s cubic-bezier(.25,.46,.45,.94) both'}
+		animation_out={'slide-out 1s cubic-bezier(.25,.46,.45,.94) both'}
+		top={200}
+		bottom={50}
+	>
 		<Projects />
-		<!-- <Blog /> -->
-		<Contact />
-	</div>
+	</Saos>
+	<!-- <Blog /> -->
+	<Contact />
+</div>
 
-	<Footer />
-</main>
+<style>
+	@keyframes -global-slide-top {
+		0% {
+			transform: translateY(200px);
+		}
+		100% {
+			transform: translateY(0px);
+		}
+	}
+	@keyframes -global-slide-out {
+		0% {
+			transform: translateY(0px);
+		}
+		100% {
+			transform: translateY(200px);
+		}
+	}
+</style>
